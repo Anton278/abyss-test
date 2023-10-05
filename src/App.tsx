0f12b385a4218450adce8e46ccc42d99aa0f371a
Anton Nakonechnyi <nakonechnyi.an@gmail.com>
2023-10-05 17:57:31 +0300

    add nodes dynamically on top level

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,8 @@ import {
   MdAdd,
   MdEdit,
   MdClose,
+  MdCancel,
+  MdCheck,
 } from "react-icons/md";
 
 import Header from "./components/Header";
@@ -12,28 +14,24 @@ import Header from "./components/Header";
 import s from "./App.module.scss";
 import { generateNum } from "./utils/generateNum";
 
+type TreeNode = {
+  name: string;
+  children?: TreeNode[];
+};
+
 function App() {
   const [zoom, setZoom] = useState("100");
-  const [data, setData] = useState([
-    {
-      name: "Category 1",
-      children: [
-        { name: "sub category 1", children: [{ name: "sub sub category 1" }] },
-        { name: "sub category 2" },
-      ],
-    },
-    {
-      name: "Category 2",
-    },
-    {
-      name: "Category 3",
-    },
-  ]);
+  const [data, setData] = useState<TreeNode[]>([]);
+  const [inputVal, setInputVal] = useState("");
 
   function getTree(data: any[]) {
+    if (!data.length) {
+      return null;
+    }
+
     const res = (
       <ul>
-        {data.map((node) => {
+        {data.map((node, i) => {
           const key = generateNum.next().value as number;
 
           let childrenNodes;
@@ -42,21 +40,55 @@ function App() {
           }
 
           return (
-            <li key={key}>
+            <li key={i}>
               <div className={s.node}>
-                <p>{node.name}</p>
+                {node.name ? (
+                  <p>{node.name}</p>
+                ) : (
+                  <input
+                    type="text"
+                    value={inputVal}
+                    onChange={(e) => setInputVal(e.target.value)}
+                  />
+                )}
                 <div className={s.nodeActions}>
-                  <button className={s.nodeActionButton}>
-                    <MdAdd />
-                  </button>
-                  <button className={s.nodeActionButton}>
-                    <MdEdit />
-                  </button>
-                  <button
-                    className={[s.nodeActionButton, s.redBackground].join(" ")}
-                  >
-                    <MdClose />
-                  </button>
+                  {node.name ? (
+                    <>
+                      <button className={s.nodeActionButton}>
+                        <MdAdd />
+                      </button>
+                      <button className={s.nodeActionButton}>
+                        <MdEdit />
+                      </button>
+                      <button
+                        className={[s.nodeActionButton, s.redBackground].join(
+                          " "
+                        )}
+                      >
+                        <MdClose />
+                      </button>
+                    </>
+                  ) : (
+                    <>
+                      <button
+                        className={[
+                          s.nodeActionButton,
+                          s.yellowBackground,
+                        ].join(" ")}
+                        onClick={() => cancelAddNode(i)}
+                      >
+                        <MdClose />
+                      </button>
+                      <button
+                        className={[s.nodeActionButton, s.greenBackground].join(
+                          " "
+                        )}
+                        onClick={() => approveAddNode(i)}
+                      >
+                        <MdCheck />
+                      </button>
+                    </>
+                  )}
                 </div>
               </div>
               {childrenNodes}
@@ -70,6 +102,21 @@ function App() {
   }
 
   const tree = getTree(data);
+
+  function addChildNode() {
+    setData([...data, { name: "" }]);
+  }
+  function cancelAddNode(rmIndex: number) {
+    setData((oldData) => oldData.filter((_, i) => i !== rmIndex));
+  }
+  function approveAddNode(addingNodeIndex: number) {
+    setData((oldData) =>
+      oldData.map((node, i) =>
+        i === addingNodeIndex ? { name: inputVal } : node
+      )
+    );
+    setInputVal("");
+  }
 
   return (
     <>
@@ -93,7 +140,10 @@ function App() {
               <div className={[s.topNode, s.node].join(" ")}>
                 <p>Categories</p>
                 <div className={s.nodeActions}>
-                  <button className={s.nodeActionButton}>
+                  <button
+                    className={s.nodeActionButton}
+                    onClick={() => addChildNode()}
+                  >
                     <MdAdd />
                   </button>
                 </div>
